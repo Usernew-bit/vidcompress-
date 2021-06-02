84528315c94c6e2534ab832ec1485f99b912803e
Usernew-bit <84923161+Usernew-bit@users.noreply.github.com>
2021-06-02 20:58:58 +0530

    Update new_join_fn.py

--- bot/plugins/new_join_fn.py
+++ bot/plugins/new_join_fn.py
@@ -29,10 +29,10 @@ async def help_message_f(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Updates Channel', url='https://t.me/AyushBots')
+                    InlineKeyboardButton('owner', url='https://t.me/I_45H_07')
                 ],
                 [
-                    InlineKeyboardButton('Source Code', url='https://github.com/CyberBoyAyush/VideoCompress')
+                    InlineKeyboardButton('Group', url='https://t.me/grpsprt')
                 ]
             ]
         ),
